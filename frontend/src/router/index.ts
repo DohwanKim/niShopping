621ca3117ff0df329cb63239c82c4b5eb3d29bf9
DohwanKim <windboy2004@naver.com>
2019-11-05 21:02:23 +0900

    fix: fouter/index.ts

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Main from '../views/MainView.vue';
-import AuthService from '../service/AuthService';
-import store from '../store'
+import UserMenuView from '../views/UserMenuView.vue';
+import UserInfoView from '../views/user/InfoView.vue';
 
 Vue.use(VueRouter);
 
@@ -11,9 +11,16 @@ const routes = [
     path: '/',
     name: 'Main',
     component: Main,
-    meta: {
-      requireAuth:true
-    }
+  },
+  {
+    path: '/user/',
+    name: 'UserMenuView',
+    component: UserMenuView,
+  },
+  {
+    path: '/user/info',
+    name: 'UserInfoView',
+    component: UserInfoView,
   },
 ];
 
@@ -21,26 +28,6 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
-});
-
-const authService = new AuthService();
-
-router.beforeEach(async (to, from, next)=>{
-  var user = await authService.getUser();
-  if(to.path === '/callback'){
-    await authService.siginCallback();
-    next('/')
-  }
-  if(to.path === '/logout'){
-    await authService.signOutCallback();
-    await authService.signIn();
-  }
-  if(to.meta.requireAuth && user==null){
-    document.location.href = "https://dev-z1noh6-s.auth0.com/v2/logout?returnTo=http%3A%2F%2Flocalhost%3A8080%logout"
-  }
-  // store.commit('userInfoChange', user);
-  // console.log("test",store.state.userInfo);
-  next();
 });
 
 export default router;
