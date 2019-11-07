c7ad121c32b0bcbdf8fe10c1414c72861dd912a8
dyjung123 <jeongdongryun@gmail.com>
2019-11-07 19:47:24 +0900

    fix: add lang="ts" to script tag

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -115,10 +115,12 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Footer',
-};
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class Footer extends Vue {
+}
 </script>
 
 <style scoped>
