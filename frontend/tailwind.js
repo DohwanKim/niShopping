d9832f723d9843156cb4f09276a7597e8d5bb32d
dyjung123 <jeongdongryun@gmail.com>
2019-11-07 15:51:38 +0900

    feature: add footer component

--- frontend/tailwind.js
+++ frontend/tailwind.js
@@ -1,11 +1,30 @@
 module.exports = {
   theme: {
+    maxWidth: {
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+      '3/5': '60%',
+      'lg': '1023px',
+    },
+    minWidth: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
     extend: {
       colors: {
         'appleBlack': '#333333',
-      }
-    }
+        'appleFooter': '#f5f5f7',
+        'appleGray': {
+          '100': '#515154',
+          '200': '#86868b',
+          '300': '#999999',
+        },
+      },
+    },
   },
   variants: {},
-  plugins: []
-}
+  plugins: [],
+};
