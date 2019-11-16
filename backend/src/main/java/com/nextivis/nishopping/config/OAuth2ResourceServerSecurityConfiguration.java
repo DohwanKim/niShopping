2802eefbb1e08ce5ba6418f3ec69cd02a89ab752
deokgoo <kkddgg1001@gmail.com>
2019-11-16 18:00:06 +0900

    resourceServer end

--- backend/src/main/java/com/nextivis/nishopping/config/OAuth2ResourceServerSecurityConfiguration.java
+++ backend/src/main/java/com/nextivis/nishopping/config/OAuth2ResourceServerSecurityConfiguration.java
@@ -8,15 +8,18 @@ import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.*;
 import org.springframework.security.oauth2.jwt.*;
 
+@EnableWebSecurity
 public class OAuth2ResourceServerSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") String jwkSetUri;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
+        http.csrf();
         http.authorizeRequests(authorizeRequests ->
                 authorizeRequests
-                               .antMatchers(HttpMethod.GET, "/api/user/**").hasAnyAuthority("")
+                               .antMatchers(HttpMethod.GET, "/api/user/**").permitAll()
                                .anyRequest().authenticated()
         ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
